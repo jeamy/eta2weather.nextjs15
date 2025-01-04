a21ae1975dbfc062bc948e87ecf7820079626297
jeamy lee <jeamy.lee@gmail.com>
2025-01-04 19:00:10 +0100

    cache values

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
-
-};
-
-
-export default nextConfig;
+    experimental: {
+      // Remove any experimental features that are no longer supported
+      serverActions: true,
+    },
+    // Add any other necessary configuration options here
+  }
+  
+  module.exports = nextConfig
+  
